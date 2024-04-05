b9b19e38c899a36803f7b91cc1d7c27ca4f0a590
battikerigayathri <battikerigayathri2454@gmail.com>
2024-04-05 16:44:40 +0530

    changes in asset model

--- src/app/api/graphql/models/Assets.ts
+++ src/app/api/graphql/models/Assets.ts
@@ -1,18 +1,21 @@
 import mercury from "@mercury-js/core";
 export const Asset = mercury.createModel("Asset", {
-    name: {
-        type: "string",
-    },
-    type: {
-        type: "string",
-    },
-    path: {
-        type: "string"
-    },
-    altText: {
-        type: "string"
-    },
-    description: {
-        type: "string"
-    }
+  name: {
+    type: "string",
+  },
+  type: {
+    type: "enum",
+    enumType: "string",
+    enum: ["MEDIA", "WEBCONFIG"],
+    default: "MEDIA",
+  },
+  path: {
+    type: "string",
+  },
+  altText: {
+    type: "string",
+  },
+  description: {
+    type: "string",
+  },
 });
