2002237e4ee62e6953bb4f47cb2fdc2159320237
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2024-04-11 12:27:59 +0530

    blog model remodeled

--- src/app/api/graphql/models/index.ts
+++ src/app/api/graphql/models/index.ts
@@ -1,6 +1,5 @@
 export {Asset} from "./Assets"
 export {Blog} from "./Blog"
 export {Category} from "./Category"
-export {SubCategory} from "./SubCategory"
 export {User} from "./User"
 export { Setting } from "./Setting";
