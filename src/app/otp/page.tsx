404ac21b18eebd9cc9b59c57bb4042d4e399a093
Avinash Koduri <saiavinashkoduri@gmail.com>
2024-04-23 18:37:26 +0530

    created otp and change password pages

---
+++ src/app/otp/page.tsx
@@ -0,0 +1,10 @@
+import OTP from '@/container/otp'
+import React from 'react'
+
+function page() {
+  return (
+    <div><OTP/></div>
+  )
+}
+
+export default page
---
+++ src/app/resetpassword/page.tsx
@@ -0,0 +1,10 @@
+import ResetPassword from '@/container/resetPasswordContainer'
+import React from 'react'
+
+function page() {
+  return (
+    <div><ResetPassword/></div>
+  )
+}
+
+export default page
