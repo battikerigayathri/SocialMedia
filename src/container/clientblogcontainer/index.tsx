389e6b667e0c4c21f51ffb611415af33c8a25095
thrinetra <thrinetravikram@gmail.com>
2024-05-02 17:53:03 +0530

    clientv iewnlogpage

--- src/container/clientblogcontainer/index.tsx
+++ src/container/clientblogcontainer/index.tsx
@@ -171,7 +171,7 @@ const Clientblogview = () => {
         where: {
           status: "PUBLISH",
         },
-        limit: 4,
+        limit: 3,
         sort: {
           createdOn: "asc",
         },
@@ -196,43 +196,63 @@ const Clientblogview = () => {
     const year = date.getFullYear();
     return `${day}-${month}-${year}`;
   }
+  function archivedates({ createdon }: { createdon: any }) {
+    const date = new Date(createdon);
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const month = monthNames[date.getMonth()];
+    const year = date.getFullYear();
+    return `${month}-${year}`;
+  }
 
   return (
-    <div className="p-5 flex flex-row">
+    <div className="p-5 flex flex-row bg-gray-100 gap-2  ">
       <div>
-        <div className="flex flex-row gap-2">
-          <Link href={mylatestblogs?.data?.listBlogs.docs[0].slug || "#"}>
-            <div className="w-[650px] h-[400px] border-gray-500 border-[1px] shadow-sm">
-              <div>
-                <text className="font-bold">
-                  {mylatestblogs?.data?.listBlogs.docs[0]?.titile}
-                </text>
+        <div className="flex flex-row gap-4 bg-white rounded-md ">
+          <div className="relative group hover:scale-105 ease-in duration-300 md:flex flex-row justify-center">
+            <Link href={mylatestblogs?.data?.listBlogs.docs[0].slug || "#"}>
+              <div className=" w-[650px] h-[400px] shadow-sm">
+                <div className="absolute top-2 left-2">
+                  <text className=" bg-gray-700 font-sm text-white rounded-md p-1 bg-opacity-75">
+                    {mylatestblogs?.data?.listBlogs.docs[0]?.title}
+                  </text> 
+                </div>
+                <Image
+                  className=" w-full h-full object-center"
+                  src={mylatestblogs?.data?.listBlogs?.docs[0]?.thumbnail?.path}
+                  alt="image"
+                  height={1000}
+                  width={1000}
+                />
               </div>
-              <div>
-                <text className="font-bold">
-                  {mylatestblogs?.data?.listBlogs.docs[0]?.titile}
-                </text>
-              </div>
-              <Image
-                className=" w-650 h-400 object-cover"
-                src={mylatestblogs?.data?.listBlogs.docs[0]?.thumbnail?.path}
-                alt="image"
-                height={1000}
-                width={1000}
-              />
-            </div>
-          </Link>
-          <div className="flex flex-col gap-5">
+            </Link>
+          </div>
+          <div className=" flex flex-wrap relative group hover:scale-105 ease-in duration-300 ">
             {mylatestblogs?.data?.listBlogs.docs
               ?.slice(1)
               .map((item: any, index: any) => (
                 <Link
                   href={item.slug || "#"}
-                  className="w-[300px] h-[120px] border-gray-500 border-[1px] shadow-sm p-1 text-left"
+                  className="w-[300px] h-[200px] object-fill shadow-sm  text-left "
                 >
+                  <text className=" absolute bg-gray-700 font-sm text-white rounded-md p-1 bg-opacity-75 top-2 left-2">
+                    {mylatestblogs?.data?.listBlogs.docs[1]?.title}
+                  </text>
                   <Image
-                    className=" object-cover object-top w-full h-full"
-                    src={item?.thumbnail?.path}
+                    className=" w-full h-full object-center"
+                    src={mylatestblogs?.data?.listBlogs?.docs[0]?.thumbnail?.path}
                     alt="image"
                     height={1000}
                     width={1000}
@@ -246,51 +266,58 @@ const Clientblogview = () => {
         </div>
         <div className="grid grid-cols-3">
           {mypinposts?.data?.listBlogs.docs.map((item: any, index: any) => (
-            <div className="w-[280px] h-[450px] border-gray-500 border-[1px] border-b-2">
-              <div>
-                <Image
-                  className=" w-80 h-60 object-cover"
-                  src={item?.thumbnail?.path}
-                  alt="image"
-                  height={1000}
-                  width={1000}
-                />
-              </div>
-              <div className="p-3">
-                <div className="flex flex-col gap-[1px]">
-                  <text className="font-bold text-blue-400">{item?.title}</text>
-                  <div className="flex flex-row gap-4">
-                    <text className="text-blue-400">
-                      {item?.author?.firstName
-                        ? item?.author?.firstName
-                        : "not Available"}
+            <Link href={item.slug || "#"}>
+              <div className="w-[280px] h-[450px] border-gray-500 bg-white rounded-md relative group hover:scale-105 ease-in duration-200">
+                <div>
+                  <Image
+                    className=" w-80 h-60 object-center"
+                    src={item?.thumbnail?.path}
+                    alt="image"
+                    height={1000}
+                    width={1000}
+                  />
+                </div>
+                <div className="p-3 ">
+                  <div className="flex flex-col gap-[1px]">
+                    <text className="font-bold text-blue-400">
+                      {item?.title}
                     </text>
-                    <text className="text-blue-400">
-                      {" "}
-                      {item?.createdOn
-                        ? formatDate({ createdon: item?.createdOn })
-                        : "no date"}
-                    </text>
+                    <div className="flex flex-row gap-4">
+                      <text className="text-blue-400">
+                        {item?.author?.firstName
+                          ? item?.author?.firstName
+                          : ""}
+                      </text>
+                      <text className="text-blue-400">
+                        {" "}
+                        {item?.createdOn
+                          ? formatDate({ createdon: item?.createdOn })
+                          : "no date"}
+                      </text>
+                    </div>
+                    <p className="text-sm line-clamp-6 pt-2 justify-center text-pretty">
+                      {item?.description}
+                    </p>
                   </div>
-                  <p className="text-sm line-clamp-6 pt-2 justify-center text-pretty">
-                    {item?.description}
-                  </p>
                 </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
-      <div className="flex flex-col gap-5">
-        <div className="border-gray-400 border-[1px] w-[250px] h-[400px] p-3">
-          <text>Recent Posts</text>
+      <div className="flex flex-col gap-5 ">
+        <div className="border-gray-400  w-[260px] h-[400px] p-3 bg-white rounded-md">
+          <text>Featured Posts</text>
           {data?.listBlogs.docs.map((item: any, index: number) => {
             console.log(item, "item");
             return (
-              <Link href={item.slug || "#"} className="flex flex-row  border-gray-500  gap-2 mt-2">
-                <div className="w-[120px] h-[70px] border-[1px]">
+              <Link
+                href={item.slug || "#"}
+                className="flex flex-row  border-gray-500  gap-2 mt-2"
+              >
+                <div className="w-[140px] h-[80px] border-[1px]">
                   <Image
-                    className=" w-full h-full object-cover"
+                    className=" w-full h-full object-center"
                     src={item?.thumbnail?.path}
                     alt="image"
                     height={70}
@@ -299,27 +326,38 @@ const Clientblogview = () => {
                 </div>
                 <div className="flex flex-col gap-[1px]">
                   <text className="font-light">{item?.title}</text>
-                  <text className="font-light text-sm">
+                  {/* <text className="font-light text-sm">
                     {item?.author?.firstName
                       ? item?.author?.firstName
-                      : "NotAvailable"}
+                      : ""}
                   </text>
                   <text className="font-light text-sm ">
                     {item?.createdOn
                       ? formatDate({ createdon: item?.createdOn })
-                      : "no date"}
-                  </text>
+                      : "no date"} */}
+                  {/* </text> */}
                 </div>
               </Link>
             );
           })}
-          <div className="ml-40">
-            <text className="text-blue-400">View all</text>
-          </div>
         </div>
 
-        <div className="border-gray-400 border-[1px] w-[250px] h-[250px] p-3">
+        <div className="border-gray-400 bg-white rounded-md w-[260px] h-[250px] p-3">
           <text>Archives</text>
+          <div>
+            {data?.listBlogs.docs.map((item: any, index: number) => {
+              console.log(data, "checkdate");
+              return (
+                <Link href={'#'}> 
+                <text className=" text-md pb-3 text-blue-300 font-medium ">
+                  {item?.createdOn
+                    ? archivedates({ createdon: item?.createdOn })
+                    : "no date"}
+                </text>
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens:{
+        'sm':{max:"468px"},
+
+      },
     },
   },
   plugins: [
