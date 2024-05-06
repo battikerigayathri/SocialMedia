16f19ae8533036df5646ef39e9b884ddba81afb4
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2024-05-06 18:59:32 +0530

    bug fixes

--- src/app/api/graphql/hooks/blog.hook.ts
+++ src/app/api/graphql/hooks/blog.hook.ts
@@ -2,21 +2,23 @@ import mercury from "@mercury-js/core";
 
 mercury.hook.before("CREATE_BLOG_RECORD", function (this: any) {
   console.log(JSON.stringify(this.options.args.input));
-    const title = this.options.args.input.title;
-    const slug = title.toString().toLowerCase().replace(/[^a-z0-9-]/g, '');
-    this.options.args.input.slug = slug
+  const title = this.options.args.input.title;
+  let slug = title
     .toString()
     .toLowerCase()
     .replaceAll(" ", "-");
+  slug.toString().toLowerCase().replace(/[^a-z0-9-]/g, '');
+  this.options.args.input.slug = slug;
 });
 
 mercury.hook.before("UPDATE_BLOG_RECORD", function (this: any) {
   const title = this.options?.args?.input?.title;
   if (title) {
-    const slug = title.toString().toLowerCase().replace(/[^a-z0-9-]/g, '');
-    this.options.args.input.slug = slug
+    let slug = title
       .toString()
       .toLowerCase()
       .replaceAll(" ", "-");
+    slug.toString().toLowerCase().replace(/[^a-z0-9-]/g, '');
+    this.options.args.input.slug = slug;
   }
 });
