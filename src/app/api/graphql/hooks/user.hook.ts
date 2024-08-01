dced90590355fe884ae5743c363b9bcf8e895b49
battikerigayathri <battikerigayathri2454@gmail.com>
2024-08-01 13:27:45 +0530

    changes

--- src/app/api/graphql/hooks/user.hook.ts
+++ src/app/api/graphql/hooks/user.hook.ts
@@ -1,6 +1,4 @@
 import mercury from "@mercury-js/core";
-
-// Hook before creating a user record
 mercury.hook.before("CREATE_USER_RECORD", async function (this: any) {
   const { userName, firstName, lastName, email } = this.options.args.input;
 
@@ -16,22 +14,21 @@ mercury.hook.before("CREATE_USER_RECORD", async function (this: any) {
     });
     for (const user of existingUsers) {
       if (user.userName === userName) {
-        throw new Error(`A user with the username "${userName}" already exists.`);
-      }
-      if (user.email === email) {
-        throw new Error(`A user with the email "${email}" already exists.`);
+        throw new Error(`User with the username "${userName}" already exists.`);
       }
       if (user.firstName === firstName) {
-        throw new Error(`A user with the first name "${firstName}" already exists.`);
+        throw new Error(`User with the first name "${firstName}" already exists.`);
       }
       if (user.lastName === lastName) {
-        throw new Error(`A user with the last name "${lastName}" already exists.`);
+        throw new Error(`User with the last name "${lastName}" already exists.`);
       }
-      
+      if (user.email === email) {
+        throw new Error(`User with the email "${email}" already exists.`);
+      }
     }
     
   } catch (error: any) {
     console.error("Error creating user:", error);
-    throw new Error("Unable to create user. " + error.message);
+    throw new Error(error.message);
   }
 });
